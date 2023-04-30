fa93b479703aaa5659d27049f00d9deb51ed8bf2
Nnouka Stephen <nnoukastephen@gmail.com>
2023-04-30 19:20:50 +0200

    feat: Landing page - Manipulate the DOM
    - Programmatically build navigation
    
    - Scroll to anchors from navigation
    
    - Highlights section in viewport upon scrolling
    
    - Check performance

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Landing Page Project
 
-## Table of Contents
+## Activities
 
-* [Instructions](#instructions)
+- Manipulate the DOM
 
-## Instructions
+- Programmatically build navigation
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+- Scroll to anchors from navigation
 
-To get started, open `js/app.js` and start building out the app's functionality
+- Highlights section in viewport upon scrolling
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+- Check performance
+
+
+[View app.js](./js/app.js)
+
+© [UDACITY](https://udacity.com)
+
+
--- js/app.js
+++ js/app.js
@@ -23,6 +23,13 @@
  * 
 */
 
+// Application data store
+const store = {
+    sections: [],
+    links: {},
+    scrollTimerId: -1,
+    pageHeader: null,
+}
 
 /**
  * End Global Variables
@@ -30,7 +37,99 @@
  * 
 */
 
+/**
+ * Create Menu link
+ */
+function MenuLink({ label, href, onClick }) {
+    const link = document.createElement('a');
+    link.href = href;
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        onClick?.();
+    });
+    link.classList.add('menu__link');
+    link.append(label);
+    return link;
+}
+/**
+ * Create Nav Menu Item
+ */
+function MenuItem({ label, href, onClick }) {
+    const item = document.createElement('li');
+    item.appendChild(MenuLink({ label, href, onClick }));
+    return item;
+}
 
+/**
+ * Add an element to a Document root element
+ */
+function DOMRoot(selector) {
+    try {
+        let entry = document.querySelector(selector);
+        function render(element) {
+            if (entry && element) {
+                entry.append(element);
+            }
+        }
+        return { render }
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+/**
+ * Select app sections
+ */
+function selectAppSections() {
+    return document.querySelectorAll('[data-nav]');
+}
+
+/**
+ * show/hide app header
+ */
+function showHideHeader() {
+    if (store.pageHeader == null) {
+        store.pageHeader = document.querySelector('#page__header');
+    }
+    store.pageHeader.classList.remove('hidden_header');
+    clearTimeout(store.scrollTimerId);
+    store.scrollTimerId = setTimeout(() => {
+        if (window.scrollY) {
+            store.pageHeader.classList.add('hidden_header');
+        }
+    }, 1000);
+}
+
+/**
+ * Calculate position and add/remove active classes
+ */
+function addActiveClasses() {
+    // get the least y > 0;
+    const toArr = Array.prototype.slice.call(store.sections);
+    toArr.sort(function (a, b) {
+        const aR = a.getBoundingClientRect();
+        const bR = b.getBoundingClientRect();
+        const aY = aR.y + aR.height / 4;
+        const bY = bR.y + bR.height / 4;
+        if (aY === bY) return 0;
+        else if (aY < 0 && bY < 0) return aY > bY ? -1 : 1;
+        else if (aY < 0) return 1;
+        else if (bY < 0) return -1;
+        else if (aY < bY) return -1;
+
+    });
+    toArr.forEach((s, i) => {
+        if (i == 0) {
+            s.classList.add('active-section');
+            store.links[s.id]?.classList.add('active-link');
+        }
+        else {
+            s.classList.remove('active-section');
+            store.links[s.id]?.classList.remove('active-link');
+        }
+    });
+    showHideHeader();
+}
 
 /**
  * End Helper Functions
@@ -38,13 +137,38 @@
  * 
 */
 
-// build the nav
+/**
+ * Man application entry point
+ */
+function Main() {
+    store.sections = selectAppSections();
+    // build the nav
+    const navFragment = document.createDocumentFragment();
+    // Build menu 
+    store.sections.forEach(s => {
+        const menuItem = MenuItem({
+            label: s.getAttribute('data-nav'),
+            href: `#${s.id}`,
+            onClick() {
+                // Scroll to anchor ID using scrollTO event
+                // Scroll to section on link click
+                window.scrollTo({
+                    top: s.getBoundingClientRect().top + window.scrollY,
+                    behavior: 'smooth'
+                });
+            }
+        });
+        navFragment.appendChild(menuItem);
+        store.links[s.id] = menuItem;
+    });
+    // Add class 'active' to section when near top of viewport
+    addActiveClasses();
+    // Set sections as active
+    document.addEventListener('scroll', addActiveClasses);
+    // render the nav
+    DOMRoot('#navbar__list').render(navFragment);
+}
 
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
 
 
 /**
@@ -53,10 +177,5 @@
  * 
 */
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+// start application
+document.addEventListener('DOMContentLoaded', Main);
